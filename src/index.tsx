844139e2ea7a0e934bcdf524c4858b9baef0bb74
valdeirsbs <valdeirsbs@gmail.com>
2021-12-04 20:41:39 -0300

    listing transations

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,28 @@ createServer({
   models: {
     transaction: Model
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freela',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Freela',
+          type: 'withdraw',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        }
+      ]
+    })
+  },
 
   routes() {
     this.namespace = 'api';
