75b29d54cd954e99c9bde884953d0ae9237deb28
valdeirsbs <valdeirsbs@gmail.com>
2021-12-04 16:48:15 -0300

    added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+`
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
