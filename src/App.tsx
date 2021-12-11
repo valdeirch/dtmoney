5652294669c5a2557089f0faae5f2d716c25973f
valdeirsbs <valdeirsbs@gmail.com>
2021-12-11 15:46:56 -0300

    added context

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
+import { TransactionsContext } from './TransactionsContext';
 
 
 export function App() {
@@ -17,7 +18,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -27,6 +28,6 @@ export function App() {
       />
       
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
